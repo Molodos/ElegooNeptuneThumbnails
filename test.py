70870776a6f69e19c99ceaa13c7731a1781d1221
Molodos <michael-weichenrieder@t-online.de>
2023-05-31 11:28:34 +0200

    Prepared for multiple text line display

--- test.py
+++ test.py
@@ -7,14 +7,31 @@ from PyQt6 import QtWidgets
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QImage, QPainter, QFont
 
-app = QtWidgets.QApplication(sys.argv)
-image = QImage(900, 900, QImage.Format.Format_ARGB32)
-painter = QPainter(image)
-painter.fillRect(0, 0, 900, 900, Qt.GlobalColor.gray)
-font = QFont("Arial", 60)
-painter.setFont(font)
-painter.setPen(Qt.GlobalColor.darkGreen)
-painter.drawText(0, 0, 880, 890, Qt.AlignmentFlag.AlignRight + Qt.AlignmentFlag.AlignBottom, "12:07")
-painter.drawText(0, 0, 880, 890, Qt.AlignmentFlag.AlignRight + Qt.AlignmentFlag.AlignBottom, "12:07")
-painter.end()
-image.save("test.png")
+
+class Test:
+
+    @classmethod
+    def test(cls) -> None:
+        app = QtWidgets.QApplication(sys.argv)
+        image = QImage(900, 900, QImage.Format.Format_ARGB32)
+        painter = QPainter(image)
+        painter.fillRect(0, 0, 900, 900, Qt.GlobalColor.darkGray)
+        painter.end()
+        image = cls.add_text(image, ["0.15€", "11.4m", "1:03h"])
+        image.save("test.png")
+
+    @classmethod
+    def add_text(cls, image: QImage, text: list[str]) -> QImage:
+        painter = QPainter(image)
+        font = QFont("Arial", 60)
+        painter.setFont(font)
+        painter.setPen(Qt.GlobalColor.darkGreen)
+        for i, line in enumerate(reversed(text)):
+            painter.drawText(0, 0, 880, 890 - (i * 110), Qt.AlignmentFlag.AlignRight + Qt.AlignmentFlag.AlignBottom,
+                             line)
+        painter.end()
+        return image
+
+
+if __name__ == "__main__":
+    Test.test()
