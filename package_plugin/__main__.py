a53f56d9b386a75f581e740c72335013a2be6bc7
Molodos <michael-weichenrieder@t-online.de>
2023-07-09 17:44:34 +0200

    Minor fixes

--- package_plugin/__main__.py
+++ package_plugin/__main__.py
@@ -10,6 +10,7 @@ PACKAGE_PATH: str = os.path.dirname(os.path.realpath(__file__))
 PLUGIN_FILES: list[str] = ["__init__.py", "elegoo_neptune_thumbnails.py", "bg_thumbnail.png", "LICENSE", "plugin.json", "README.md", "changelog.txt"]
 
 BUILD_NAME = os.path.join(PACKAGE_PATH, "ElegooNeptuneThumbnails.curapackage")
+PLUGIN_BUILD_NAME = os.path.join(PACKAGE_PATH, "ElegooNeptuneThumbnails.zip")
 
 PACKAGE_JSON_PATH = os.path.join(PACKAGE_PATH, "package.json")
 
@@ -22,12 +23,19 @@ if __name__ == "__main__":
     if os.path.exists(BUILD_NAME):
         os.remove(BUILD_NAME)
 
-    # Clone base build
+    # Clone base builds
     shutil.copyfile(os.path.join(PACKAGE_PATH, "base.curapackage"), BUILD_NAME)
+    shutil.copyfile(os.path.join(PACKAGE_PATH, "base.zip"), PLUGIN_BUILD_NAME)
 
-    # Add plugin files
+    # Add plugin files to package
     with ZipFile(BUILD_NAME, 'a') as file:
-        file.write(PACKAGE_JSON_PATH, "package.json")
+        file.write(PACKAGE_JSON_PATH, "/package.json")
         for plugin_file in PLUGIN_FILES:
             file.write(os.path.join(PACKAGE_PATH, "..", plugin_file),
                        f"/files/plugins/ElegooNeptune3Thumbnails/{plugin_file}")
+
+    # Add plugin files to plugin (ultimaker upload)
+    with ZipFile(PLUGIN_BUILD_NAME, 'a') as file:
+        for plugin_file in PLUGIN_FILES:
+            file.write(os.path.join(PACKAGE_PATH, "..", plugin_file),
+                       f"/ElegooNeptune3Thumbnails/{plugin_file}")
