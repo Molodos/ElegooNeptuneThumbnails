d7e7a16bccba95af431451a216f86a1c767c3f18
Molodos <michael-weichenrieder@t-online.de>
2023-12-19 14:37:22 +0100

    Fixed a bug, that caused the plugin to crash on loading, when not running on Windows

--- tools/thumbnail_generator.py
+++ tools/thumbnail_generator.py
@@ -3,7 +3,7 @@
 
 import math
 from array import array
-from ctypes import CDLL, windll
+from ctypes import CDLL
 from os import path
 
 from PyQt6.QtCore import Qt, QByteArray, QBuffer, QIODeviceBase
@@ -243,7 +243,6 @@ class ThumbnailGenerator:
         """
         img_type = f";{img_type}:"
 
-        windll.kernel32.SetDllDirectoryW(None)
         if Platform.isOSX():
             p_dll = CDLL(path.abspath(path.join(path.dirname(__file__), "..", "libs", "libColPic.dylib")))
         elif Platform.isLinux():
