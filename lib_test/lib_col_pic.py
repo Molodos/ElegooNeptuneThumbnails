bda977537134bdad56c133f64557a2c918511180
Molodos <michael-weichenrieder@t-online.de>
2023-12-22 03:15:45 +0100

    Added a POC for a pure python image encoding

--- lib_test/lib_col_pic.py
+++ lib_test/lib_col_pic.py
@@ -1,39 +1,262 @@
-class ColorList:
+def ColPic_EncodeStr(fromcolor16, picw, pich, outputdata: bytearray, outputmaxtsize, colorsmax):
+    qty = 0
+    temp = 0
+    strindex = 0
+    hexindex = 0
+    TempBytes = bytearray(4)
+    qty = ColPicEncode(fromcolor16, picw, pich, outputdata, outputmaxtsize, colorsmax)
+    if qty == 0:
+        return 0
+    temp = 3 - qty % 3
+    while temp > 0 and qty < outputmaxtsize:
+        outputdata[qty] = 0
+        qty += 1
+        temp -= 1
+
+    if qty * 4 / 3 >= outputmaxtsize:
+        return 0
+    hexindex = qty
+    strindex = qty * 4 / 3
+    while hexindex > 0:
+        hexindex -= 3
+        strindex -= 4
+        TempBytes[0] = outputdata[hexindex] >> 2
+        TempBytes[1] = outputdata[hexindex] & 3
+        TempBytes[1] <<= 4
+        TempBytes[1] += outputdata[hexindex + 1] >> 4
+        TempBytes[2] = outputdata[hexindex + 1] & 15
+        TempBytes[2] <<= 2
+        TempBytes[2] += outputdata[hexindex + 2] >> 6
+        TempBytes[3] = outputdata[hexindex + 2] & 63
+        TempBytes[0] += 48
+        if chr(TempBytes[0]) == '\\':
+            TempBytes[0] = 126
+        TempBytes[1] += 48
+        if chr(TempBytes[1]) == '\\':
+            TempBytes[1] = 126
+        TempBytes[2] += 48
+        if chr(TempBytes[2]) == '\\':
+            TempBytes[2] = 126
+        TempBytes[3] += 48
+        if chr(TempBytes[3]) == '\\':
+            TempBytes[3] = 126
+        outputdata[int(strindex)] = TempBytes[0]
+        outputdata[int(strindex) + 1] = TempBytes[1]
+        outputdata[int(strindex) + 2] = TempBytes[2]
+        outputdata[int(strindex) + 3] = TempBytes[3]
+
+    qty = qty * 4 / 3
+    outputdata[int(qty)] = 0
+    return qty
+
+
+def ColPicEncode(fromcolor16, picw, pich, outputdata: bytearray, outputmaxtsize, colorsmax):
+    l0 = U16HEAD()
+    Head0 = ColPicHead3()
+    Listu16 = []
+    for i in range(1024):
+        Listu16.append(U16HEAD())
+
+    ListQty = 0
+    enqty = 0
+    dotsqty = picw * pich
+    if colorsmax > 1024:
+        colorsmax = 1024
+    for i in range(dotsqty):
+        ListQty = ADList0(fromcolor16[i], Listu16, ListQty, 1024)
+
+    for index in range(1, ListQty):
+        l0 = Listu16[index]
+        for i in range(index):
+            if l0.qty >= Listu16[i].qty:
+                aListu16 = bListu16 = Listu16.copy()
+                for j in range(index - i):
+                    Listu16[i + j + 1] = aListu16[i + j]
+
+                Listu16[i] = l0
+                break
+
+    while ListQty > colorsmax:
+        l0 = Listu16[ListQty - 1]
+        minval = 255
+        fid = -1
+        for i in range(colorsmax):
+            cha0 = Listu16[i].A0 - l0.A0
+            if cha0 < 0:
+                cha0 = 0 - cha0
+            cha1 = Listu16[i].A1 - l0.A1
+            if cha1 < 0:
+                cha1 = 0 - cha1
+            cha2 = Listu16[i].A2 - l0.A2
+            if cha2 < 0:
+                cha2 = 0 - cha2
+            chall = cha0 + cha1 + cha2
+            if chall < minval:
+                minval = chall
+                fid = i
+
+        for i in range(dotsqty):
+            if fromcolor16[i] == l0.colo16:
+                fromcolor16[i] = Listu16[fid].colo16
+
+        ListQty = ListQty - 1
+
+    for n in range(len(outputdata)):
+        outputdata[n] = 0
+
+    Head0.encodever = 3
+    Head0.oncelistqty = 0
+    Head0.mark = 98419516
+    Head0.ListDataSize = ListQty * 2
+    outputdata[0] = 3
+    outputdata[12] = 60
+    outputdata[13] = 195
+    outputdata[14] = 221
+    outputdata[15] = 5
+    outputdata[16] = ListQty * 2 & 255
+    outputdata[17] = (ListQty * 2 & 65280) >> 8
+    outputdata[18] = (ListQty * 2 & 16711680) >> 16
+    outputdata[19] = (ListQty * 2 & 4278190080) >> 24
+    sizeofColPicHead3 = 32
+    for i in range(ListQty):
+        outputdata[sizeofColPicHead3 + i * 2 + 1] = (Listu16[i].colo16 & 65280) >> 8
+        outputdata[sizeofColPicHead3 + i * 2 + 0] = Listu16[i].colo16 & 255
+
+    enqty = Byte8bitEncode(fromcolor16, sizeofColPicHead3, Head0.ListDataSize >> 1, dotsqty, outputdata,
+                           sizeofColPicHead3 + Head0.ListDataSize,
+                           outputmaxtsize - sizeofColPicHead3 - Head0.ListDataSize)
+    Head0.ColorDataSize = enqty
+    Head0.PicW = picw
+    Head0.PicH = pich
+    outputdata[4] = picw & 255
+    outputdata[5] = (picw & 65280) >> 8
+    outputdata[6] = (picw & 16711680) >> 16
+    outputdata[7] = (picw & 4278190080) >> 24
+    outputdata[8] = pich & 255
+    outputdata[9] = (pich & 65280) >> 8
+    outputdata[10] = (pich & 16711680) >> 16
+    outputdata[11] = (pich & 4278190080) >> 24
+    outputdata[20] = enqty & 255
+    outputdata[21] = (enqty & 65280) >> 8
+    outputdata[22] = (enqty & 16711680) >> 16
+    outputdata[23] = (enqty & 4278190080) >> 24
+    return sizeofColPicHead3 + Head0.ListDataSize + Head0.ColorDataSize
+
+
+def ADList0(val, Listu16, ListQty, maxqty):
+    qty = ListQty
+    if qty >= maxqty:
+        return ListQty
+    for i in range(qty):
+        if Listu16[i].colo16 == val:
+            Listu16[i].qty += 1
+            return ListQty
+
+    A0 = val >> 11 & 31
+    A1 = (val & 2016) >> 5
+    A2 = val & 31
+    Listu16[qty].colo16 = val
+    Listu16[qty].A0 = A0
+    Listu16[qty].A1 = A1
+    Listu16[qty].A2 = A2
+    Listu16[qty].qty = 1
+    ListQty = qty + 1
+    return ListQty
+
+
+def Byte8bitEncode(fromcolor16, listu16Index, listqty, dotsqty, outputdata: bytearray, outputdataIndex, decMaxBytesize):
+    listu16 = outputdata
+    dots = 0
+    srcindex = 0
+    decindex = 0
+    lastid = 0
+    temp = 0
+    while dotsqty > 0:
+        dots = 1
+        for i in range(dotsqty - 1):
+            if fromcolor16[srcindex + i] != fromcolor16[srcindex + i + 1]:
+                break
+            dots += 1
+            if dots == 255:
+                break
+
+        temp = 0
+        for i in range(listqty):
+            aa = listu16[i * 2 + 1 + listu16Index] << 8
+            aa |= listu16[i * 2 + 0 + listu16Index]
+            if aa == fromcolor16[srcindex]:
+                temp = i
+                break
+
+        tid = int(temp % 32)
+        if tid > 255:
+            tid = 255
+        sid = int(temp / 32)
+        if sid > 255:
+            sid = 255
+        if lastid != sid:
+            if decindex >= decMaxBytesize:
+                dotsqty = 0
+                break
+            outputdata[decindex + outputdataIndex] = 7
+            outputdata[decindex + outputdataIndex] <<= 5
+            outputdata[decindex + outputdataIndex] += sid
+            decindex += 1
+            lastid = sid
+        if dots <= 6:
+            if decindex >= decMaxBytesize:
+                dotsqty = 0
+                break
+            aa = dots
+            if aa > 255:
+                aa = 255
+            outputdata[decindex + outputdataIndex] = aa
+            outputdata[decindex + outputdataIndex] <<= 5
+            outputdata[decindex + outputdataIndex] += tid
+            decindex += 1
+        else:
+            if decindex >= decMaxBytesize:
+                dotsqty = 0
+                break
+            outputdata[decindex + outputdataIndex] = 0
+            outputdata[decindex + outputdataIndex] += tid
+            decindex += 1
+            if decindex >= decMaxBytesize:
+                dotsqty = 0
+                break
+            aa = dots
+            if aa > 255:
+                aa = 255
+            outputdata[decindex + outputdataIndex] = aa
+            decindex += 1
+        srcindex += dots
+        dotsqty -= dots
+
+    return decindex
+
+
+class U16HEAD:
 
     def __init__(self):
-        self._color_list: dict[int, int] = dict()
-
-    def get_length(self) -> int:
-        return len(self._color_list)
-
-    def add_color(self, color: int) -> bool:
-        if self.get_length() < 1024:
-            self._color_list[color] = self._color_list.get(color, 0) + 1
-            return True
-        return False
-
-    def get_sorted_entries(self) -> list[tuple[int, int, int, int, int]]:
-        # Color, r, g, b, count
-        sorted_entries: list[tuple[int, int, int, int, int]] = list()
-        for color, count in sorted(self._color_list.items(), key=lambda x: x[1], reverse=True):
-            b: int = color & 255
-            g: int = (color >> 8) & 255
-            r: int = (color >> 16) & 255
-            sorted_entries.append((color, r, g, b, count))
-        return sorted_entries
+        self.colo16 = 0
+        self.A0 = 0
+        self.A1 = 0
+        self.A2 = 0
+        self.res0 = 0
+        self.res1 = 0
+        self.qty = 0
 
 
-def col_pic_encode(input_color_16: list[int], color_list: ColorList, output_max_size: int, colors_max: int) -> int:
-    colors_max = max(1024, colors_max)
-    for color in input_color_16:
-        color_list.add_color(color=color)
-    for color, r, g ,b, count in color_list.get_sorted_entries()[::-1]:
-        pass
-    return 0
+class ColPicHead3:
 
-
-def col_pic_encode_str(input_color_16: list[int], output_data: str, output_max_size: int, colors_max: int) -> int:
-    color_list: ColorList = ColorList()
-    encoded_len: int = col_pic_encode(input_color_16, color_list, output_max_size, colors_max)
-    # TODO
-    return encoded_len
+    def __init__(self):
+        self.encodever = 0
+        self.res0 = 0
+        self.oncelistqty = 0
+        self.PicW = 0
+        self.PicH = 0
+        self.mark = 0
+        self.ListDataSize = 0
+        self.ColorDataSize = 0
+        self.res1 = 0
+        self.res2 = 0
--- lib_test/test_lib.py
+++ lib_test/test_lib.py
@@ -8,71 +8,134 @@ from os import path
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QImage, QPainter
 
+from lib_test import lib_col_pic
+
 
 class TestLib:
     """
     Thumbnail generator
     """
 
-    RGB_PATH: str = path.abspath(path.join(path.dirname(path.realpath(__file__)), "rgb_3x3.png"))
     BG_BLACK: str = path.abspath(path.join(path.dirname(path.realpath(__file__)), "..", "img", "bg_old.png"))
+    BENCHY: str = path.abspath(path.join(path.dirname(path.realpath(__file__)), "..", "img", "benchy.png"))
 
     @classmethod
-    def test(cls, image_path: str, use_lib: bool, width: int = 200, height: int = 200) -> tuple[int, bytes]:
+    def test(cls, image_path: str, use_lib: bool, width: int = 200, height: int = 200, img_type: str = "gimage") -> str:
         image: QImage = QImage(3, 3, QImage.Format.Format_RGBA8888)
         painter = QPainter(image)
         painter.drawImage(0, 0, QImage(image_path))
         painter.end()
         if use_lib:
-            return cls._parse_thumbnail_new(img=image, width=width, height=height)
+            return cls._parse_thumbnail_binary(img=image, width=width, height=height, img_type=img_type)
         else:
-            return 10, b'0123425678910'
+            return cls._parse_thumbnail_python(img=image, width=width, height=height, img_type=img_type)
 
     @classmethod
-    def _parse_thumbnail_new(cls, img: QImage, width: int, height: int) -> tuple[int, bytes]:
-        # Scale image
+    def _parse_thumbnail_binary(cls, img: QImage, width: int, height: int, img_type: str) -> str:
+        img_type = f";{img_type}:"
+
+        p_dll = CDLL(path.abspath(path.join(path.dirname(__file__), "ColPic_X64.dll")))
+
+        result = ""
         b_image = img.scaled(width, height, Qt.AspectRatioMode.KeepAspectRatio)
         img_size = b_image.size()
-
+        color16 = array('H')
         try:
-            # 'H' stands for array of unsigned shorts (ushort_t*)
-            color16 = array('H')
-
-            # Encode image in array
-            for y in range(img_size.height()):
-                for x in range(img_size.width()):
-                    # Get color at index and parse to 16bit color
-                    pixel_color = b_image.pixelColor(x, y)
+            for i in range(img_size.height()):
+                for j in range(img_size.width()):
+                    pixel_color = b_image.pixelColor(j, i)
                     r = pixel_color.red() >> 3
                     g = pixel_color.green() >> 2
                     b = pixel_color.blue() >> 3
                     rgb = (r << 11) | (g << 5) | b
-
-                    # Add color to array
                     color16.append(rgb)
 
-            # Load dll
-            p_dll = CDLL(path.abspath(path.join(path.dirname(__file__), "ColPic_X64.dll")))
-
-            from_color16 = color16.tobytes()
-
-            # 'B' stands for array of unsigned chars (uchar_t*) -> converted to fixed length bytes (pixel count)
-            output_data: bytes = array('B', [0] * img_size.height() * img_size.width()).tobytes()
-
             # int ColPic_EncodeStr(U16* fromcolor16, int picw, int pich, U8* outputdata, int outputmaxtsize, int colorsmax);
+            from_color16 = color16.tobytes()
+            output_data = array('B', [0] * img_size.height() * img_size.width()).tobytes()
             result_int = p_dll.ColPic_EncodeStr(from_color16, img_size.height(), img_size.width(), output_data,
                                                 img_size.height() * img_size.width(), 1024)
-            return result_int, output_data
+
+            data0 = str(output_data).replace('\\x00', '')
+            data1 = data0[2:len(data0) - 2]
+            each_max = 1024 - 8 - 1
+            max_line = int(len(data1) / each_max)
+            append_len = each_max - 3 - int(len(data1) % each_max)
+
+            for i in range(len(data1)):
+                if i == max_line * each_max:
+                    result += '\r;' + img_type + data1[i]
+                elif i == 0:
+                    result += img_type + data1[i]
+                elif i % each_max == 0:
+                    result += '\r' + img_type + data1[i]
+                else:
+                    result += data1[i]
+            result += '\r;'
+            for j in range(append_len):
+                result += '0'
+
         except Exception as e:
             raise e
 
+        return result + '\r'
+
+    @classmethod
+    def _parse_thumbnail_python(cls, img: QImage, width: int, height: int, img_type: str) -> str:
+        img_type = f";{img_type}:"
+
+        result = ""
+        b_image = img.scaled(width, height, Qt.AspectRatioMode.KeepAspectRatio)
+        img_size = b_image.size()
+        color16 = array('H')
+        try:
+            for i in range(img_size.height()):
+                for j in range(img_size.width()):
+                    pixel_color = b_image.pixelColor(j, i)
+                    r = pixel_color.red() >> 3
+                    g = pixel_color.green() >> 2
+                    b = pixel_color.blue() >> 3
+                    rgb = (r << 11) | (g << 5) | b
+                    color16.append(rgb)
+            output_data = bytearray(img_size.height() * img_size.width() * 10)
+            result_int = lib_col_pic.ColPic_EncodeStr(color16, img_size.height(), img_size.width(), output_data,
+                                                    img_size.height() * img_size.width() * 10, 1024)
+
+            data0 = str(output_data).replace('\\x00', '')
+            data1 = data0[2:len(data0) - 2]
+            each_max = 1024 - 8 - 1
+            max_line = int(len(data1) / each_max)
+            append_len = each_max - 3 - int(len(data1) % each_max) + 10
+            j = 0
+            for i in range(len(output_data)):
+                if output_data[i] != 0:
+                    if j == max_line * each_max:
+                        result += '\r;' + img_type + chr(output_data[i])
+                    elif j == 0:
+                        result += img_type + chr(output_data[i])
+                    elif j % each_max == 0:
+                        result += '\r' + img_type + chr(output_data[i])
+                    else:
+                        result += chr(output_data[i])
+                    j += 1
+            result += '\r;'
+            for m in range(append_len):
+                result += '0'
+
+        except Exception as e:
+            raise e
+
+        return result + '\r'
+
 
 def print_data(image_path: str, use_lib: bool, width: int = 200, height: int = 200) -> None:
-    count, data = TestLib.test(image_path=image_path, use_lib=use_lib, width=width, height=height)
-    print(f"Size: {count}")
-    print(f"Data: {data[:count]}")
+    data: str = TestLib.test(image_path=image_path, use_lib=use_lib, width=width, height=height)
+    print(f"[{'Binary' if use_lib else 'Python'}] Image: {image_path}")
+    print(f"Data: {data}")
+    print()
 
 
 if __name__ == "__main__":
-    print_data(image_path=TestLib.BG_BLACK, use_lib=True)
-    print_data(image_path=TestLib.RGB_PATH, use_lib=True)
+    image: str = TestLib.BENCHY
+    print_data(image_path=image, use_lib=True)
+    print_data(image_path=image, use_lib=False)
